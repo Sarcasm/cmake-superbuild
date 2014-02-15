82b68e6355997a39a1b76e731167601bfda245f2
Guillaume Papin <guillaume.papin@epitech.eu>
2014-02-15 15:13:29 +0100

    Remove use of DOWNLOAD_NAME and rename deps directory (fix #1)
    CMake 2.8.7 doesn't provide DOWNLOAD_NAME, it wasn't needed anyway.

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -1,14 +1,13 @@
 include (ExternalProject)
 
-set_property (DIRECTORY PROPERTY EP_BASE dependencies)
+set_property (DIRECTORY PROPERTY EP_BASE Dependencies)
 
 set (DEPENDENCIES)
 set (EXTRA_CMAKE_ARGS)
 
-# Use static linking to avoid issues with the system-wide installation of Boost.
+# Use static linking to avoid issues with system-wide installations of Boost.
 list (APPEND DEPENDENCIES ep_boost)
 ExternalProject_Add (ep_boost
-  DOWNLOAD_NAME boost_1_55_0.tar.bz2
   URL http://sourceforge.net/projects/boost/files/boost/1.55.0/boost_1_55_0.tar.bz2/download
   URL_MD5 d6eef4b4cacb2183f2bf265a5a03a354
   CONFIGURE_COMMAND ./bootstrap.sh --with-libraries=atomic,date_time,filesystem,program_options,system,thread
@@ -17,7 +16,7 @@ ExternalProject_Add (ep_boost
   INSTALL_COMMAND ""
   )
 list (APPEND EXTRA_CMAKE_ARGS
-  -DBOOST_ROOT=${CMAKE_CURRENT_BINARY_DIR}/dependencies/Source/ep_boost
+  -DBOOST_ROOT=${CMAKE_CURRENT_BINARY_DIR}/Dependencies/Source/ep_boost
   -DBoost_NO_SYSTEM_PATHS=ON)
 
 # FIXME add to default target "all"?
@@ -27,6 +26,3 @@ ExternalProject_Add (ep_blah
   CMAKE_ARGS -DUSE_SUPERBUILD=OFF ${EXTRA_CMAKE_ARGS}
   INSTALL_COMMAND ""
   BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/blah)
-
-
-message (STATUS "ARGS: ${CMAKE_ARGS}, OPTIONS: ${CMAKE_OPTIONS}")
